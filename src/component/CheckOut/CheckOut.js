e373a236c6a5e5878c2154c629aa4d609caa349e
zunead <zuned1996@outlook.com>
2021-04-16 19:12:23 +0600

    fixed problem

--- src/component/CheckOut/CheckOut.js
+++ src/component/CheckOut/CheckOut.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const CheckOut = () => {
-    let {_id} = useParams();
+    const {_id} = useParams();
     console.log(useParams())
+
+    const [product, setProduct] = useState({});
+    console.log(product)
+    useEffect(() => {
+        fetch('https://fierce-coast-48195.herokuapp.com/product/' + _id)
+        .then(res => res.json())
+        .then(data => setProduct(data));
+    }, [_id])
     return (
         <div>
-            {_id}
+            id data: {_id}
+            fetch data: {product}
         </div>
     );
 };
