0561affa0c0cba1070a5294911334129ac702ebb
zunead <zuned1996@outlook.com>
2021-04-17 15:35:40 +0600

    fixed deploy port problem

--- src/component/CheckOut/CheckOut.js
+++ src/component/CheckOut/CheckOut.js
@@ -3,19 +3,29 @@ import { useParams } from 'react-router';
 
 const CheckOut = () => {
     const {_id} = useParams();
-    console.log(useParams())
+    console.log('id', _id)
 
     const [product, setProduct] = useState({});
     console.log(product)
+    
     useEffect(() => {
         fetch('https://fierce-coast-48195.herokuapp.com/product/' + _id)
+        // fetch(`http://localhost:5000/product/${_id}`)
         .then(res => res.json())
         .then(data => setProduct(data));
     }, [_id])
+
+    
     return (
         <div>
-            id data: {_id}
-            fetch data: {product}
+             {
+                    product.length === 0 && <div className="spinner-border text-info spiner" role="status">
+                    <span class="sr-only">Loading...</span>
+                  </div>
+                }
+            ID: {product._id}
+            Name: {product.name}
+            price: {product.price}
         </div>
     );
 };
